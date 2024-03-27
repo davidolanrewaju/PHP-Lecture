8781afccb7967dfc204a53ad815017cfd3803664
David <davidolanrewaju538@gmail.com>
2024-03-27 11:03:08 +0100

    PHP basics

---
+++ php-basics/index.php
@@ -0,0 +1,30 @@
+<?php 
+$hello = 'Hello World!';
+// $x = 5;
+// echo $x;
+// var_dump($x);
+// $x = (string)$x;
+// echo ($x . '<br>');
+// var_dump($x);
+
+// define('greet', 'Good Morning');
+// echo greet . '<br>';
+
+// $x = 20;
+// echo ($x<10 ? 'I am less than 10' : 'I am greater than 10');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Welcome to PHP</h1>
+    <h2>
+        <?php echo $hello?>
+    </h2>
+</body>
+</html>
